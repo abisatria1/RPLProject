76db2ccd36ed1ed1008087d52079ec17feb916f2
Abisatria1 <abi.kadek@gmail.com>
2020-06-03 19:27:59 +0800

    menambahkan force database menjadi true

--- index.js
+++ index.js
@@ -16,10 +16,18 @@ app.use(bodyParser.json())
 app.use(cors())
 
 // router
-const customerRouter = require('./routes/customerRouter')
+const customerRouter = require('./routes/customerRoutes')
+const addressRouter = require('./routes/addressRoutes')
 app.use('/api/customer', customerRouter)
+app.use('/api/customer/address', addressRouter)
 
-app.use('/' , (req,res,next) => res.send('Application successfuly deploy, welcome to the API'))
+app.use('/unauthorized' , (req,res,next) => {
+    let err = new Error ('Unauthorized access')
+    err.status = 401
+    next(err)
+})
+
+app.use('/hello' , (req,res,next) => res.send('Application successfuly deploy, welcome to the API'))
 // error handling
 app.use((req,res,next) => {
     let err = new Error('Route not found')
@@ -37,7 +45,7 @@ app.use((err,req,res,next) => {
 const port = process.env.PORT || 3000
 
 app.listen(port , () => {
-    db.sync({})
+    db.sync({force : true})
     .then(() => console.log(`app is running on port ${port}`))
     .catch(err => console.log(err.message))
 }) 

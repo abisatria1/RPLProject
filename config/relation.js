b46f420ab7efd1e855654d51ab9e62764f6481b9
Abisatria1 <abi.kadek@gmail.com>
2020-07-01 22:28:35 +0800

    menambahkan relasi antara room dan kategori product

--- config/relation.js
+++ config/relation.js
@@ -1,5 +1,7 @@
 const Customer = require('../models/customer')
 const Address = require('../models/address')
+const Category = require('../models/category')
+const Room = require('../models/room')
 
 Customer.hasMany(Address)
 Address.belongsTo(Customer)

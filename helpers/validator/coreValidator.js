44664370135436ce650dbbac85fd4db1ee430457
Abisatria1 <abi.kadek@gmail.com>
2020-08-26 00:58:20 +0800

    menambahkan count total price untuk confirm order

--- helpers/validator/coreValidator.js
+++ helpers/validator/coreValidator.js
@@ -35,7 +35,39 @@ const findItemInCartByProductId = () => {
     }
 }
 
+const countTotalPrice = () => {
+    return async (req,res,next) => {
+        const {user} = req
+        const {order} = req.body
+        let total = 0
+        let err = false
+        let cartItemsId = []
+        for (let i = 0; i < order.length; i++) {
+            const element = order[i]
+            const item = await user.getCarts({
+                where : {
+                    [Op.and] : [
+                        {id : element.cartId},
+                        {orderId : {[Op.is] : null}}
+                    ]
+                },
+            })
+            if (!item.length) {
+                err = true
+                break
+            }
+            cartItemsId.push(item[0].id)
+            total += item[0].fixedPrice * item[0].quantity
+        }
+        if (err) return response(res,false,null,'Cart id is invalid',400)
+        req.body.orderPriceTotal = total
+        req.body.cartItemsId = cartItemsId
+        next()
+    }
+}
+
 module.exports = {
     validateProductAddToCart,
-    findItemInCartByProductId
+    findItemInCartByProductId,
+    countTotalPrice
 }
